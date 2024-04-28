c94a3d492ed264cdb6d7724054c1186183d0002d
TiagoGB <tiago.bannwart@gmail.com>
2024-04-28 17:48:21 -0300

    Initial Commit

---
+++ sv-02.py
@@ -0,0 +1,11 @@
+import http.server
+
+class server(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'sv-02')
+
+servidor = http.server.HTTPServer(('localhost', 80), server)
+servidor.serve_forever()
---
+++ test_lb.py
@@ -0,0 +1,22 @@
+import requests
+import time
+
+# ip = '35.208.9.186'
+ip = 'localhost'
+
+requests_total = 10
+interval_seconds = 1
+
+count = {
+    'sv-01': 0,
+    'sv-02': 0,
+}
+
+for i in range(requests_total):
+    x = requests.get(f'http://{ip}/')
+    count[x.text] += 1
+
+    print(x.text)
+    time.sleep(interval_seconds)
+
+print(count)
